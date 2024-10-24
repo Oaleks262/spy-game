a9990310d9e8d451fd6f6c3bdf82f2f6e3b91d16
Oleksandr Zvirych <110517733+Oaleks262@users.noreply.github.com>
2024-10-24 22:54:58 +0300

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -26,7 +26,7 @@ const Game = () => {
   const playerName = location.state?.playerName || 'Без імені';
 
   useEffect(() => {
-    ws.current = new WebSocket('ws://spy-server.onrender.com');
+    ws.current = new WebSocket('wss://spy-server.onrender.com');
 
     ws.current.onopen = () => {
       console.log('Connected to WebSocket');
