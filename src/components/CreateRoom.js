efa3dbb637b00eebdf33482fb32fa55fe2a9dd88
Oleksandr Zvirych <110517733+Oaleks262@users.noreply.github.com>
2024-10-24 22:43:52 +0300

    Update CreateRoom.js

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -11,7 +11,7 @@ const CreateRoom = () => {
   const navigate = useNavigate();
 
   const createWebSocketConnection = () => {
-    const socket = new WebSocket('ws://localhost:5000');
+    const socket = new WebSocket('ws://spy-server.onrender.com');
 
     socket.onopen = () => {
       console.log('Підключено до сервера WebSocket');
